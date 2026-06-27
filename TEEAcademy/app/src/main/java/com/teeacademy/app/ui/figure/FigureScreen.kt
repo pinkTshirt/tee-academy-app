e8226fea58102e178cdbdf5f1a7142e7b6dd423d
Dhanya <dhanvis235@gmail.com>
2026-06-28 02:08:44 +0530

    Add import for setValue in FigureScreen.kt

--- TEEAcademy/app/src/main/java/com/teeacademy/app/ui/figure/FigureScreen.kt
+++ TEEAcademy/app/src/main/java/com/teeacademy/app/ui/figure/FigureScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
