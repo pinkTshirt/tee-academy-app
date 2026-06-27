35fed20444943411c409366c31196b6bba539b96
Dhanya <dhanvis235@gmail.com>
2026-06-28 01:54:29 +0530

    Update Kotlin and KSP plugin versions

--- TEEAcademy/build.gradle.kts
+++ TEEAcademy/build.gradle.kts
@@ -1,11 +1,11 @@
 // Top-level build file
 plugins {
     id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("com.google.devtools.ksp") version "2.0.20-1.0.25" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+    id("com.google.devtools.ksp") version "2.2.21-2.0.5" apply false
     id("com.google.dagger.hilt.android") version "2.58" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
 }
 
 tasks.register("clean", Delete::class) {
